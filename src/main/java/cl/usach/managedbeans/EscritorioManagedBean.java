632338cff28b9aa47907c050538ada7bd722b6fa
freddy.guzman <freddy.guzman@usach.cl>
2014-06-02 09:57:03 -0400

    Merge commit '554067366c7a3f1a76d5ad85ae47094b0ba5eb9e'
    Conflicts:
    	src/main/java/cl/usach/gettrello/MainClass.java
    
    Comit Merge FGT Main
    
    BurndownChart Alumnos
    Nuevas Clases
    Sprint Grupos
    Sprint Asignaturas

--- src/main/java/cl/usach/managedbeans/EscritorioManagedBean.java
+++ src/main/java/cl/usach/managedbeans/EscritorioManagedBean.java
@@ -12,15 +12,24 @@ import cl.usach.entities.Lista;
 import cl.usach.entities.Miembro;
 import cl.usach.entities.Tarjeta;
 import cl.usach.entities.Usuario;
+import cl.usach.kanbanizesessionbeans.ActividadKanbanizeLocal;
+import cl.usach.kanbanizesessionbeans.ListaKanbanizeLocal;
+import cl.usach.kanbanizesessionbeans.MiembroKanbanizeLocal;
+import cl.usach.kanbanizesessionbeans.TarjetaKanbanizeLocal;
 import cl.usach.sessionbeans.DetalleUsuarioTarjetaFacadeLocal;
 import cl.usach.sessionbeans.EquipoFacadeLocal;
 import cl.usach.sessionbeans.ListaFacadeLocal;
+import cl.usach.sessionbeans.MiembroFacadeLocal;
 import cl.usach.sessionbeans.UsuarioFacadeLocal;
 import cl.usach.trellosessionbeans.ActividadTrelloLocal;
 import cl.usach.trellosessionbeans.ListaTrelloLocal;
 import cl.usach.trellosessionbeans.MiembroTrelloLocal;
 import cl.usach.trellosessionbeans.TarjetaTrelloLocal;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -30,6 +39,14 @@ import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+import org.joda.time.DateTime;
+import org.joda.time.Days;
+import org.joda.time.Hours;
+import org.joda.time.Minutes;
+import org.joda.time.Period;
+import org.joda.time.Seconds;
+import org.joda.time.format.PeriodFormatter;
+import org.joda.time.format.PeriodFormatterBuilder;
 import org.primefaces.model.DashboardColumn;
 import org.primefaces.model.DashboardModel;
 import org.primefaces.model.DefaultDashboardColumn;
@@ -43,6 +60,16 @@ import org.primefaces.model.chart.PieChartModel;
 @Named(value = "escritorioManagedBean")
 @ViewScoped
 public class EscritorioManagedBean {
+    @EJB
+    private MiembroFacadeLocal miembroFacade;
+    @EJB
+    private ActividadKanbanizeLocal actividadKanbanize;
+    @EJB
+    private TarjetaKanbanizeLocal tarjetaKanbanize;
+    @EJB
+    private ListaKanbanizeLocal listaKanbanize;
+    @EJB
+    private MiembroKanbanizeLocal miembroKanbanize;
     @EJB
     private ListaFacadeLocal listaFacade;
     @EJB
@@ -63,10 +90,12 @@ public class EscritorioManagedBean {
     private final String loginUsuario = FacesContext.getCurrentInstance().getExternalContext().getRemoteUser();
     private DashboardModel model;
     private PieChartModel pieResumenTareas;
+    private PieChartModel pieResumenHorasTareas;
     
     private List<Equipo> equipos;
     private List<Tarjeta> tarjetas; 
     private Map<String,Number> grafResumenTarea;
+    private Map<String,Number> grafResumenHorasTarea;
     
     public EscritorioManagedBean() {
     }
@@ -102,6 +131,14 @@ public class EscritorioManagedBean {
     public PieChartModel getPieResumenTareas() {
         return pieResumenTareas;
     }
+
+    public Map<String, Number> getGrafResumenHorasTarea() {
+        return grafResumenHorasTarea;
+    }
+
+    public PieChartModel getPieResumenHorasTareas() {
+        return pieResumenHorasTareas;
+    }
     
     @PostConstruct
     public void init(){
@@ -117,32 +154,39 @@ public class EscritorioManagedBean {
         column1.addWidget("proyectos");        
         column1.addWidget("tareas");
         column2.addWidget("grafTareas");
+        column2.addWidget("grafHorasTareas");
         
         model.addColumn(column1);
         model.addColumn(column2);
     }
     
-    public void buscarDatos(){
+    public void buscarDatos() throws IOException{        
         Usuario usuario = usuarioFacade.buscarPorLogin(loginUsuario);
         List<Equipo> equiposBuscar = equipoFacade.buscarPorUsuario(usuario);
-        
+
         for (Equipo equipo : equiposBuscar) {
             if("Trello".equals(equipo.getIdCuenta().getIdTipoCuenta().getNombreTipoCuenta())){                
                 //Obtener o Actualizar Miembros
                 miembroTrello.buscarMiembros(equipo);
-                
                 //Obtener o Actualizar Listas
-                listaTrello.buscarListaPorTablero(equipo);              
-                       
+                listaTrello.buscarListaPorTablero(equipo);
                 //Obtener o Actualizar Tarjetas
                 tarjetaTrello.buscarTarjetasPorLista(equipo);
-                
                 //Obtener Actividades
                 actividadTrello.buscarActividades(equipo);
             }
-            
+            if("Kanbanize".equals(equipo.getIdCuenta().getIdTipoCuenta().getNombreTipoCuenta())){
+                //Obtener o actualizar Miembros
+                miembroKanbanize.buscarMiembos(equipo);
+                //Obtener o Actualizar Listas
+                listaKanbanize.buscarListas(equipo);
+                //Obtener o Actualizar Tarjetas
+                tarjetaKanbanize.buscarTarjetas(equipo);
+                //Obtener Actividades
+                actividadKanbanize.buscarActividades(equipo);
+            }
         }
-        actualizarEscritorio();        
+        actualizarEscritorio();
         FacesMessage msg = new FacesMessage("Datos Actualizados","");  
         FacesContext.getCurrentInstance().addMessage(null, msg);
     }
@@ -155,13 +199,14 @@ public class EscritorioManagedBean {
     public void buscarTareas(){
         List<Tarjeta> tar = new ArrayList<>();
         Map<String,Number> mapProy = new HashMap<>();
+        Map<String,Number> mapHoras = new HashMap<>();       
         for (Equipo equipo : equipos) {            
             Lista ultimaLista = listaFacade.buscarUltimaPorTablero(equipo.getIdTablero());
-            List<Miembro> miembros = equipo.getIdCuenta().getMiembroList();
+            List<Miembro> miembros = miembroFacade.buscarPoIdTableroYIdCuenta(equipo.getIdTablero(),equipo.getIdCuenta());
             for (Miembro miembro : miembros) {
                 List<DetalleUsuarioTarjeta> detalles = 
-                        detalleUsuarioTarjetaFacade.buscarPorIdMiembroYIdTableroYNoLista(miembro,equipo.getIdTablero(),ultimaLista);
-                for (DetalleUsuarioTarjeta detalle : detalles) {                    
+                        detalleUsuarioTarjetaFacade.buscarPorIdMiembroYIdTableroYNoLista(miembro,equipo.getIdTablero(),ultimaLista); 
+                for (DetalleUsuarioTarjeta detalle : detalles) {
                     tar.add(detalle.getIdTarjeta());                   
                     
                     if(mapProy.containsKey(detalle.getIdTarjeta().getIdTablero().getNombreTablero())){
@@ -170,11 +215,39 @@ public class EscritorioManagedBean {
                     }else{
                         mapProy.put(detalle.getIdTarjeta().getIdTablero().getNombreTablero(),1);
                     }
+                    
+                    if(detalle.getIdTarjeta().getIdEstadoTarjeta().getNombreEstadoTarjeta().equals("En proceso")){
+                        if(mapHoras.containsKey(detalle.getIdTarjeta().getIdTablero().getNombreTablero())){
+                            if(detalle.getIdTarjeta().getIdEstadoTarjeta().getNombreEstadoTarjeta().equals("Terminado")){
+                                int tiempo = obtenerSegundosDiff(detalle.getIdTarjeta().getFechaInicioTarjeta(),
+                                        detalle.getIdTarjeta().getFechaFinalTarjeta());
+                                Number sum = mapHoras.get(detalle.getIdTarjeta().getIdTablero().getNombreTablero()).intValue() + tiempo;
+                                mapHoras.put(detalle.getIdTarjeta().getIdTablero().getNombreTablero(), sum);
+                            }else{
+                                int tiempo = obtenerSegundosDiff(detalle.getIdTarjeta().getFechaInicioTarjeta()
+                                        ,new Date());
+                                Number sum = mapHoras.get(detalle.getIdTarjeta().getIdTablero().getNombreTablero()).intValue() + tiempo;
+                                mapHoras.put(detalle.getIdTarjeta().getIdTablero().getNombreTablero(), sum);
+                            }
+                        }else{
+                            if(detalle.getIdTarjeta().getIdEstadoTarjeta().getNombreEstadoTarjeta().equals("Terminado")){
+                                int tiempo = obtenerSegundosDiff(detalle.getIdTarjeta().getFechaInicioTarjeta(),
+                                        detalle.getIdTarjeta().getFechaFinalTarjeta());
+                                mapHoras.put(detalle.getIdTarjeta().getIdTablero().getNombreTablero(), tiempo);
+                            }else{
+                                int tiempo = obtenerSegundosDiff(detalle.getIdTarjeta().getFechaInicioTarjeta()
+                                        ,new Date());
+                                mapHoras.put(detalle.getIdTarjeta().getIdTablero().getNombreTablero(), tiempo);
+                            }
+                        }
+                    }                    
                 }
             }            
         }
+        
         tarjetas = tar;
         grafResumenTarea = mapProy;
+        grafResumenHorasTarea = mapHoras;
     }
     
     public void crearResumenTareaGraf(){
@@ -184,9 +257,65 @@ public class EscritorioManagedBean {
         pieResumenTareas.setShowDataLabels(true);
     }
     
-    public void actualizarEscritorio(){        
+    public void crearResumenHorasTareaGraf(){
+        pieResumenHorasTareas = new PieChartModel(grafResumenHorasTarea);
+        pieResumenHorasTareas.setLegendPosition("e");
+        pieResumenHorasTareas.setShowDataLabels(true);
+    }
+    
+    public void actualizarEscritorio(){
         buscarProjectos();
         buscarTareas();
         crearResumenTareaGraf();
+        crearResumenHorasTareaGraf();
     }
+    
+    public int obtenerSegundosDiff(Date inicio, Date fin){
+        DateTime dtI = new DateTime(inicio);
+        DateTime dtF = new DateTime(fin);        
+        int tiempo = Seconds.secondsBetween(dtI, dtF).getSeconds(); 
+        return tiempo;
+    }
+    
+    public String obtenerTiempo(Date inicio){
+        if(inicio == null) return "-";
+        DateTime dtI = new DateTime(inicio);
+        DateTime dtF = new DateTime(new Date());
+        String tiempo = "";
+        String aux;
+        if(Days.daysBetween(dtI, dtF).getDays() > 0) tiempo += Days.daysBetween(dtI, dtF).getDays() + " d, ";
+        aux = Hours.hoursBetween(dtI, dtF).getHours() % 24 + ":";
+        if(aux.length() == 2) aux = "0"+aux;
+        tiempo += aux;
+        aux = Minutes.minutesBetween(dtI, dtF).getMinutes() % 60 + "";
+        if(aux.length() == 1) aux = "0"+aux;
+        tiempo += aux + " hrs";
+        return tiempo;
+    }
+    
+    public String obtenerTiempoProyecto(String nombre){
+        String tiempo = "-";
+        if(grafResumenHorasTarea.containsKey(nombre)){
+            Number numero = grafResumenHorasTarea.get(nombre);
+            long secondsTime = numero.longValue();
+            int segundos = (int) secondsTime % 60;
+            secondsTime -= segundos;
+            long minutesTime = secondsTime / 60;
+            long minutos = minutesTime % 60;
+            minutesTime -= minutos;
+            long hoursTime = minutesTime / 60;
+            long horas = hoursTime % 24;
+            hoursTime -= horas;
+            long dias = hoursTime / 24;
+            tiempo = "" + dias + " dia(s), " + horas + " hora(s), " + minutos +  " minuto(s), " + segundos + " segundo(s)";
+        }        
+        return tiempo;
+    }
+    
+    public String formatFecha(Date fecha){
+        if(fecha == null) return "-";        
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        String datef = df.format(fecha);
+        return datef;
+    }   
 }
---
+++ src/main/java/cl/usach/managedbeans/GraficoManagedBean.java
@@ -0,0 +1,198 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cl.usach.managedbeans;
+
+import cl.usach.entities.Equipo;
+import cl.usach.entities.Tarjeta;
+import cl.usach.entities.Usuario;
+import cl.usach.sessionbeans.EquipoFacadeLocal;
+import cl.usach.sessionbeans.TarjetaFacadeLocal;
+import cl.usach.sessionbeans.UsuarioFacadeLocal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import org.joda.time.DateTime;
+import org.joda.time.Days;
+import org.joda.time.DurationFieldType;
+import org.joda.time.LocalDate;
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.CartesianChartModel;
+import org.primefaces.model.chart.CategoryAxis;
+import org.primefaces.model.chart.ChartSeries;
+import org.primefaces.model.chart.LineChartModel;
+import org.primefaces.model.chart.LineChartSeries;
+
+/**
+ *
+ * @author FGT
+ */
+@Named(value = "graficoManagedBean")
+@ViewScoped
+public class GraficoManagedBean {
+    @EJB
+    private TarjetaFacadeLocal tarjetaFacade;
+    @EJB
+    private EquipoFacadeLocal equipoFacade;
+    @EJB
+    private UsuarioFacadeLocal usuarioFacade;
+
+    private List<Equipo> equipos; 
+    
+    private final String loginUsuario = FacesContext.getCurrentInstance().getExternalContext().getRemoteUser();
+    private final SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+    
+    public GraficoManagedBean() {
+    }
+    
+    @PostConstruct
+    public void init(){
+        buscarProjectos();
+    }
+    
+    public List<Equipo> getEquipos() {
+        return equipos;
+    }
+
+    public void setEquipos(List<Equipo> equipos) {
+        this.equipos = equipos;
+    }
+   
+    public void buscarProjectos(){
+        Usuario usuario = usuarioFacade.buscarPorLogin(loginUsuario);
+        equipos = equipoFacade.buscarPorUsuario(usuario);
+    }
+    
+    public LineChartModel crearGraficos(Equipo equipo){
+        LineChartModel lineModelAux = buscarDatosGraficos(equipo);
+        lineModelAux.setTitle(equipo.getIdTablero().getNombreTablero());
+        lineModelAux.setLegendPosition("e");
+        lineModelAux.setShowPointLabels(true);
+        lineModelAux.getAxes().put(AxisType.X, new CategoryAxis("Días"));
+        Axis yAxis = lineModelAux.getAxis(AxisType.Y);
+        yAxis.setLabel("Tareas");
+        yAxis.setMin(0);
+        return lineModelAux;
+    }
+    
+    public LineChartModel buscarDatosGraficos(Equipo equipo){
+        List<Tarjeta> tarjetas = tarjetaFacade.buscarPorTablero(equipo.getIdTablero());
+        LineChartModel model = new LineChartModel();
+        Collections.sort(tarjetas);
+        Map<String,Number> valores = inicializarMAP(tarjetas.get(0).getFechaCreacionTarjeta(), new Date());
+        if(valores != null){
+            for (Tarjeta tarjeta : tarjetas) {           
+                for (Map.Entry<String, Number> valor : valores.entrySet()) {                
+                    if(verificarSiCuenta(valor.getKey(), tarjeta.getFechaCreacionTarjeta(), tarjeta.getFechaFinalTarjeta())){
+                        int i = valores.get(valor.getKey()).intValue() + 1;
+                        valores.put(valor.getKey(), i);
+                    }                
+                }
+            }
+            Map<String,Number> valoresGraf = new TreeMap<>(valores);
+
+            String fechaAux;
+
+            ChartSeries serie1 = new ChartSeries();
+            serie1.setLabel("Cantidad");
+            ChartSeries serie2 = new ChartSeries();
+            serie2.setLabel("Ideal");
+            int i = 0;
+            for (Map.Entry<String, Number> valorG : valoresGraf.entrySet()) {
+                fechaAux = valorG.getKey().substring(8,10)+ "-" + valorG.getKey().substring(5,7) + "-" + valorG.getKey().substring(0, 4);
+                serie1.set(fechaAux, valorG.getValue().intValue());
+                
+                if(i == 0 ){
+                    serie2.set(fechaAux, valorG.getValue().intValue());
+                }else if (i == (valoresGraf.size()-1)) {
+                    serie2.set(fechaAux, 0);
+                }
+                
+                i++;
+            }
+            
+            model.addSeries(serie1);
+            model.addSeries(serie2);
+        }
+        return model;
+    }
+    
+    public Map<String,Number> inicializarMAP(Date fechai, Date fechaf){
+        Map<String,Number> valores = new HashMap<>();
+        
+        try {
+            fechai = formatter.parse(formatter.format(fechai));
+            fechaf = formatter.parse(formatter.format(fechaf));
+            
+            DateTime datei = new DateTime(fechai);
+            DateTime datef = new DateTime(fechaf);
+
+            int dias = Days.daysBetween(datei, datef).getDays();
+            for(int i = 0; i <= dias; i++){
+                DateTime d = datei.withFieldAdded(DurationFieldType.days(), i);            
+                valores.put(formatter.format(d.toDate()), 0);
+            }            
+            return valores;
+            
+        } catch (ParseException ex) {
+            Logger.getLogger(GraficoManagedBean.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }       
+    }
+    
+    public Boolean verificarSiCuenta(String fechaCheck, Date fechaCreacion, Date fechaFinal){
+        Date fechaChk;
+        
+        if(fechaFinal != null){
+            String date1 = formatter.format(fechaCreacion);
+            String date2 = formatter.format(fechaFinal);
+
+            try {
+                fechaChk = formatter.parse(fechaCheck);
+                fechaCreacion = formatter.parse(date1);
+                fechaFinal = formatter.parse(date2);
+                                
+                if((fechaChk.after(fechaCreacion) || fechaChk.equals(fechaCreacion))
+                        && (fechaChk.before(fechaFinal))){
+                    return true;
+                }
+            } catch (ParseException ex) {
+                Logger.getLogger(GraficoManagedBean.class.getName()).log(Level.SEVERE, null, ex);
+            } 
+        }else{
+            String date1 = formatter.format(fechaCreacion);
+
+            try {
+                fechaChk = formatter.parse(fechaCheck);
+                fechaCreacion = formatter.parse(date1);
+                
+                if((fechaChk.after(fechaCreacion) || fechaChk.equals(fechaCreacion))){
+                    return true;
+                }
+            } catch (ParseException ex) {
+                Logger.getLogger(GraficoManagedBean.class.getName()).log(Level.SEVERE, null, ex);
+            } 
+        }
+        
+        return false;
+    }
+    
+}
